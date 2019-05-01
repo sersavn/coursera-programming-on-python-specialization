02ce1c4ddafb6fb6c09a4c92ed2b3dbcf11f0318
sesap <nooooooooob@yandex.ru>
2019-05-01 14:59:01 +0300

    assignments for the week2 added

--- Course2/Week2/assignments/ABC_inheretance.py
+++ Course2/Week2/assignments/ABC_inheretance.py
@@ -1,3 +1,5 @@
+#my solution of the assignment
+
 '''
 В этом задании вам даны 3 класса A, B, C, имеющие сходный (но не одинаковый) интерфейс.
 Вам необходимо создать абстрактный базовый класс Base и построить корректную схему наследования.
@@ -13,20 +15,28 @@ It is recommended to manually test code before submitting solution and follow PE
 '''
 
 import math
+from abc import ABC, abstractmethod
 
+class Base(ABC):
+    '''
+    variable 'data' takes list of float elements
+    each element lay in range (0.0, 1.0)
 
-class Base:
+    variable 'result' takes list of integer elements
+    each element lay in range [0, 1]
 
-    pass
-
-class A:
-
+    len(result) == len(data)
+    '''
     def __init__(self, data, result):
         self.data = data
         self.result = result
 
+    '''
+    @abstractmethod нужно было использовать по заданию
+    '''
+    @abstractmethod
     def get_answer(self):
-          return [int(x >= 0.5) for x in self.data]
+        return [int(x >= 0.5) for x in self.data]
 
     def get_score(self):
           ans = self.get_answer()
@@ -37,35 +47,34 @@ class A:
         return sum([(x - y) * (x - y) for (x, y) in zip(self.data,
                    self.result)])
 
-
-class B:
-
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
+class A(Base):
 
     def get_answer(self):
-          return [int(x >= 0.5) for x in self.data]
+        return [int(x >= 0.5) for x in self.data]
 
-    def get_score(self):
-          ans = self.get_answer()
-          return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
-              / len(ans)
+
+class B(Base):
+
+    def get_answer(self):
+        return [int(x >= 0.5) for x in self.data]
 
     def get_loss(self):
         return -sum([y * math.log(x) + (1 - y) * math.log(1 - x)
                     for (x, y) in zip(self.data, self.result)])
 
-    def get_pre(self):
+    def calc_res(self):
         ans = self.get_answer()
         res = [int(x == 1 and y == 1) for (x, y) in zip(ans,
                self.result)]
+        return res
+
+    def get_pre(self):
+        ans = self.get_answer()
+        res = self.calc_res()
         return sum(res) / sum(ans)
 
     def get_rec(self):
-        ans = self.get_answer()
-        res = [int(x == 1 and y == 1) for (x, y) in zip(ans,
-               self.result)]
+        res = self.calc_res()
         return sum(res) / sum(self.result)
 
     def get_score(self):
@@ -74,19 +83,10 @@ class B:
         return 2 * pre * rec / (pre + rec)
 
 
-class C:
-
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
+class C(Base):
 
     def get_answer(self):
-          return [int(x >= 0.5) for x in self.data]
-
-    def get_score(self):
-          ans = self.get_answer()
-          return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
-              / len(ans)
+        return [int(x >= 0.5) for x in self.data]
 
     def get_loss(self):
         return sum([abs(x - y) for (x, y) in zip(self.data,
